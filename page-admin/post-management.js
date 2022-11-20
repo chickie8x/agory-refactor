c659710ad1b40e69ec4b1d48db0a970077844efd
chickie8x <chickie8x.qn@gmail.com>
2022-11-21 01:31:48 +0700

    save state

--- page-admin/post-management.js
+++ page-admin/post-management.js
@@ -37,4 +37,51 @@ $(document).ready(function () {
         collapse = !collapse;
     });
 
+
+    //post management section functions
+
+    const mainTabObj = {
+        'post-view': $('#main-tab-1'),
+        'partner-invitation': $('#main-tab-2'),
+        'cost-check': $('#main-tab-3'),
+        'corporation-partner': $('#main-tab-4')
+
+    }
+
+    let activeTabBtn = $('#post-view')
+
+    $('.btn-tab-item').click(function(){
+        let tabId = $(this)[0].id
+        $('.main-tab-item').each(function(){
+            $(this).addClass('hide-item')
+        })
+        let tabContent = mainTabObj[tabId]
+        tabContent.removeClass('hide-item')
+        activeTabBtn.removeClass('btn-tab-active')
+        $(this).addClass('btn-tab-active')
+        activeTabBtn = $(this)
+    })
+
+    const subTabObj = {
+        'sub-tab-btn-1': $('#sub-tab-content-1'),
+        'sub-tab-btn-2': $('#sub-tab-content-2'),
+        'sub-tab-btn-3': $('#sub-tab-content-3'),
+        'sub-tab-btn-4': $('#sub-tab-content-4')
+    }
+
+    let activeSubTabBtn = $('#sub-tab-btn-1')
+
+    $('.tab-btn-item').click(function(){
+        let tabId = $(this)[0].id
+        console.log(tabId)
+        $('.sub-tab-item').each(function(){
+            $(this).addClass('hide-item')
+        })
+        let tabContent = subTabObj[tabId]
+        tabContent.removeClass('hide-item')
+        activeSubTabBtn.removeClass('tab-btn-active')
+        $(this).addClass('tab-btn-active')
+        activeSubTabBtn = $(this)
+    })
+
 });
