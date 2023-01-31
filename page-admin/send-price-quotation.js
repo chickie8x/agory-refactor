20a74ec149de0e28173bc64d2f4f43f4a6d5716c
chickie8x <chickie8x.qn@gmail.com>
2023-01-31 16:53:57 +0700

    save state

---
+++ page-admin/send-price-quotation.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+	$('.camp-payment-method-item').click(function() {
+		$('.camp-payment-method-item').removeClass('camp-payment-method-item-active')
+		$('.camp-payment-method-item > input').each(function() {
+			$(this).prop('checked', false)
+		})
+		$('.camp-payment-method-item > img').each(function() {
+			$(this).removeClass('svg-color-change')
+		})
+		$(this).children().first().prop('checked',true)
+		$(this).children().eq('1').addClass('svg-color-change')
+		$(this).addClass('camp-payment-method-item-active')
+	})
+
+})
