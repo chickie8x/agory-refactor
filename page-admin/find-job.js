4837b99496884bc96652dc3f67af4881c1d53f86
chickie8x <chickie8x.qn@gmail.com>
2023-02-06 11:28:59 +0700

    fix popup background transparent and position move to right

--- page-admin/find-job.js
+++ page-admin/find-job.js
@@ -6,23 +6,23 @@ $(document).ready(function () {
 	}
 
 	$('.job-item-col-title-fix-cost').click(function() {
-		$('#find-job-fix-cost-popup').addClass('popup-show')
+		$('#find-job-fix-cost-popup').addClass('find-job-popup-show')
 	})
 
 	$('.job-item-col-title-fix-list').click(function() {
-		$('#find-job-fix-list-popup').addClass('popup-show')
+		$('#find-job-fix-list-popup').addClass('find-job-popup-show')
 	})
 
 	$('.close-find-job-popup-btn, .find-job-popup-content-header > span').click(function() {
-		$('.find-job-popup').removeClass('popup-show')
+		$('.find-job-popup').removeClass('find-job-popup-show')
 	})
 
 	$('.find-job-user-avt, .find-job-user-username').click(function(){
-		$('#popup-profile').addClass('popup-show')
+		$('#popup-profile').addClass('find-job-popup-show')
 	})
 
 	$('#close-profile-popup-btn').click(function(){
-		$('#popup-profile').removeClass('popup-show')
+		$('#popup-profile').removeClass('find-job-popup-show')
 
 	})
 
