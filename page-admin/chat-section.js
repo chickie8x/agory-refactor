9e195f9e246967de26ac3371a8fb21e7c26551d9
unknown <chickie8x.qn@gmail.com>
2023-05-19 01:01:48 +0700

    save state

---
+++ page-admin/chat-section.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+
+    $('.contact-item').click(function(){
+        $('.contact-item').css('background', 'none')
+        $(this).css('background', '#E4E4EB')
+    })
+})
