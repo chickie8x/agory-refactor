bf8f27c93cc7c1df4be6e93eff28621ccd058288
chickie8x <chickie8x.qn@gmail.com>
2023-05-22 01:25:39 +0700

    save state

--- page-admin/chat-section.js
+++ page-admin/chat-section.js
@@ -4,4 +4,17 @@ $(document).ready(function () {
         $('.contact-item').css('background', 'none')
         $(this).css('background', '#E4E4EB')
     })
+
+    $('#sidebar-search-btn').click(function(){
+        $('.sidebar-default-view').addClass('hide-item')
+        $('.sidebar-mes-event').addClass('sidebar-subview-show')
+    })
+
+    $('#mes-event-btn-back').click(function(){
+        $('.sidebar-mes-event').removeClass('sidebar-subview-show')
+        setTimeout(function(){
+            $('.sidebar-default-view').removeClass('hide-item')
+        },300)
+        
+    })
 })
